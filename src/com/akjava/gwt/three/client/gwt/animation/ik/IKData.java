9f23f80e432f8856d4f2d323157d9df1575b4ef5
aki <aki@xps>
2012-01-09 22:26:17 +0900

    add ikdata

---
+++ src/com/akjava/gwt/three/client/gwt/animation/ik/IKData.java
@@ -0,0 +1,44 @@
+package com.akjava.gwt.three.client.gwt.animation.ik;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.akjava.gwt.three.client.core.Vector3;
+
+public class IKData {
+	
+	public void setBones(String[] boneNames){
+		bones.clear();
+		for(int i=0;i<boneNames.length;i++){
+			bones.add(boneNames[i]);
+		}
+	}
+	public String getLastBoneName() {
+		return lastBoneName;
+	}
+	public void setLastBoneName(String lastBoneName) {
+		this.lastBoneName = lastBoneName;
+	}
+	public List<String> getBones() {
+		return bones;
+	}
+	public void setBones(List<String> bones) {
+		this.bones = bones;
+	}
+	public Vector3 getTargetPos() {
+		return targetPos;
+	}
+	public void setTargetPos(Vector3 targetPos) {
+		this.targetPos = targetPos;
+	}
+	public int getIteration() {
+		return iteration;
+	}
+	public void setIteration(int iteration) {
+		this.iteration = iteration;
+	}
+	private String lastBoneName;
+	private List<String> bones=new ArrayList<String>();;
+	private Vector3 targetPos;
+	private int iteration;
+}
