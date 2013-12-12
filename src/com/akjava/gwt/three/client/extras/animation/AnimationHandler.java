ecaa3e3699868703ef70b81be0753e371bce0e4e
aki <aki@xps>
2013-12-12 18:18:18 +0900

    update animation handler

--- src/com/akjava/gwt/three/client/extras/animation/AnimationHandler.java
+++ src/com/akjava/gwt/three/client/extras/animation/AnimationHandler.java
@@ -1,9 +1,27 @@
 package com.akjava.gwt.three.client.extras.animation;
 
+import com.akjava.gwt.three.client.core.Object3D;
 import com.akjava.gwt.three.client.gwt.animation.AnimationData;
+import com.akjava.gwt.three.client.gwt.animation.AnimationHierarchyItem;
+import com.google.gwt.core.client.JsArray;
 
 public class AnimationHandler {
 
+
+
+
+
+
+
+	public final native Animation get(String name)/*-{
+	return $wnd.THREE.AnimationHandler.get(name);
+	}-*/;
+
+	
+	public final native JsArray<AnimationHierarchyItem> parse(Object3D root)/*-{
+	return $wnd.THREE.AnimationHandler.parse(root);
+	}-*/;
+	
 	
 	public static native final void update(double delta)/*-{
 	return $wnd.THREE.AnimationHandler.update(delta);
