161772feb7fd0869e3845de81c0084716c365814
aki <aki@xps>
2013-12-12 14:36:12 +0900

    update geometryutils

--- src/com/akjava/gwt/three/client/extras/GeometryUtils.java
+++ src/com/akjava/gwt/three/client/extras/GeometryUtils.java
@@ -1,7 +1,9 @@
 package com.akjava.gwt.three.client.extras;
 
 import com.akjava.gwt.three.client.core.Geometry;
+import com.akjava.gwt.three.client.math.Vector3;
 import com.akjava.gwt.three.client.objects.Mesh;
+import com.google.gwt.user.client.ui.CustomButton.Face;
 
 public class GeometryUtils {
 
@@ -18,11 +20,40 @@ public class GeometryUtils {
 	return geo.clone();
 	}-*/;
 
+	public static native final void merge(Geometry geo,Geometry geo2,int materialIndexOffset)/*-{
+	return $wnd.THREE.GeometryUtils.merge(geo,geo2,materialIndexOffset);
+	}-*/;
+	
 	public static native final void merge(Geometry geo,Geometry geo2)/*-{
 	return $wnd.THREE.GeometryUtils.merge(geo,geo2);
 	}-*/;
 	
+	public static native final void merge(Geometry geo,Mesh mesh,int materialIndexOffset)/*-{
+	return $wnd.THREE.GeometryUtils.merge(geo,mesh,materialIndexOffset);
+	}-*/;
+	
 	public static native final void merge(Geometry geo,Mesh mesh)/*-{
 	return $wnd.THREE.GeometryUtils.merge(geo,mesh);
+	}-*/;
+	
+	public static final native Vector3 randomPointInTriangle(Vector3 vectorA, Vector3 vectorB, Vector3 vectorC )/*-{
+	return $wnd.THREE.GeometryUtils.randomPointInTriangle(vectorA, vectorB, vectorC );
+	}-*/;
+	
+	public static final native Vector3 randomPointInFace(Face face,Geometry geometry,boolean useCachedAreas)/*-{
+	return $wnd.THREE.GeometryUtils.randomPointInFace(face,geometry,useCachedAreas);
+	}-*/;
+	
+	public static final native Vector3 randomPointsInGeometry(Geometry geometry,int n)/*-{
+	return $wnd.THREE.GeometryUtils.randomPointsInGeometry(geometry,n);
+	}-*/;
+	
+	public static final native double triangleArea(Vector3 vectorA, Vector3 vectorB, Vector3 vectorC )/*-{
+	return $wnd.THREE.GeometryUtils.triangleArea(vectorA, vectorB, vectorC );
+	}-*/;
+	
+	
+	public static final native void triangulateQuads(Geometry geometry)/*-{
+	$wnd.THREE.GeometryUtils.triangulateQuads(geometry);
 	}-*/;
 }
