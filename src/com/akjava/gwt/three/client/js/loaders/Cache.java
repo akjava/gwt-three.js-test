cd69d03ea8eae08c880125dda4707cab59998f45
aki <aki@xps>
2014-07-08 16:11:39 +0900

    for 67 Cache - Created class for caching files.

---
+++ src/com/akjava/gwt/three/client/js/loaders/Cache.java
@@ -0,0 +1,72 @@
+/*
+ * gwt-wrap three.js
+ * 
+ * Copyright (c) 2013 akimisaki3@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ 
+ based Three.js r63
+ https://github.com/mrdoob/three.js
+ The MIT License
+
+Copyright (c) 2010-2013 three.js Authors. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+  
+ */
+package com.akjava.gwt.three.client.js.loaders;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+
+public class Cache extends JavaScriptObject{
+	protected Cache() {
+	}
+
+public final native JavaScriptObject getFiles()/*-{
+return this.files;
+}-*/;
+
+public final native void setFiles(JavaScriptObject files)/*-{
+this.files = files;
+}-*/;
+
+public final native void add(String key,JavaScriptObject file)/*-{
+this.add(key,file);
+}-*/;
+
+public final native JavaScriptObject get(String key)/*-{
+return this.get(key);
+}-*/;
+
+public final native void remove(String key)/*-{
+this.remove(key);
+}-*/;
+
+public final native void clear()/*-{
+this.clear();
+}-*/;
+
+
+}
