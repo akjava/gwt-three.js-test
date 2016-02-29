e8cc392170f46c0164660158f4d75a714efa515d
Aki Miyazaki <akimisaki3@gmail.com>
2016-02-29 16:20:43 +0900

    for r71 add DirectGeometry

---
+++ src/com/akjava/gwt/three/client/js/core/DirectGeometry.java
@@ -0,0 +1,8 @@
+package com.akjava.gwt.three.client.js.core;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+//TODO implement field & method
+public class DirectGeometry  extends JavaScriptObject{
+protected DirectGeometry(){}
+}
