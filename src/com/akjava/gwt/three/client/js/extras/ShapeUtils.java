e73a075212e02d86fca13a0884bac594177a542f
Aki Miyazaki <akimisaki3@gmail.com>
2016-03-06 16:14:31 +0900

    for r73 Moved Utils to ShapeUtils

--- src/com/akjava/gwt/three/client/js/extras/ShapeUtils.java
+++ src/com/akjava/gwt/three/client/js/extras/ShapeUtils.java
@@ -1,4 +1,4 @@
-package com.akjava.gwt.three.client.js.extras.core;
+package com.akjava.gwt.three.client.js.extras;
 
 import com.akjava.gwt.three.client.js.math.Vector2;
 import com.google.gwt.core.client.JavaScriptObject;
